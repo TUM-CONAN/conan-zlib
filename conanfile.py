0dba1aa10f68bad1edf4e9e3acdf8dee42d54611
Ulrich Eck <ueck@net-labs.de>
2023-09-22 13:22:00 +0200

    copy conancenter index version to fix windows-store problem

--- conanfile.py
+++ conanfile.py
@@ -1,60 +1,110 @@
-from conans import ConanFile
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
+from conan.tools.files import apply_conandata_patches, export_conandata_patches, get, load, replace_in_file, save
+from conan.tools.scm import Version
 import os
-from conans.tools import download, unzip
-from conans import CMake
+
+required_conan_version = ">=1.53.0"
 
 
 class ZlibConan(ConanFile):
     name = "zlib"
-    version = "1.2.12"
-    ZIP_FOLDER_NAME = "zlib-%s" % version
-    generators = "cmake"
+    version = "1.3"
+    package_type = "library"
+    url = "https://github.com/TUM-CONAN/conan-zlib"
+    homepage = "https://zlib.net"
+    license = "Zlib"
+    description = ("A Massively Spiffy Yet Delicately Unobtrusive Compression Library "
+                   "(Also Free, Not to Mention Unencumbered by Patents)")
+    topics = ("zlib", "compression")
+
     settings = "os", "arch", "compiler", "build_type"
-    options = {"shared": [True, False]}
-    default_options = "shared=False"
-    exports = ["CMakeLists.txt", "FindZLIB.cmake"]
-    url="http://github.com/ulricheck/conan-zlib"
-    license="http://www.zlib.net/zlib_license.html"
-    description="A Massively Spiffy Yet Delicately Unobtrusive Compression Library (Also Free, Not to Mention Unencumbered by Patents)"
-    
-    def config(self):
-        del self.settings.compiler.libcxx 
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+    }
+
+    @property
+    def _is_mingw(self):
+        return self.settings.os == "Windows" and self.settings.compiler == "gcc"
+
+    def export_sources(self):
+        export_conandata_patches(self)
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def configure(self):
+        if self.options.shared:
+            self.options.rm_safe("fPIC")
+        self.settings.rm_safe("compiler.libcxx")
+        self.settings.rm_safe("compiler.cppstd")
+
+    def layout(self):
+        cmake_layout(self, src_folder="src")
 
     def source(self):
-        zip_name = "zlib-%s.tar.gz" % self.version
-        download("http://zlib.net/%s" % (zip_name), zip_name)
-        unzip(zip_name)
-        os.unlink(zip_name)
-        os.rename(self.ZIP_FOLDER_NAME, "sources")
+        get(self, **self.conan_data["sources"][self.version],
+            destination=self.source_folder, strip_root=True)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables["SKIP_INSTALL_ALL"] = False
+        tc.variables["SKIP_INSTALL_LIBRARIES"] = False
+        tc.variables["SKIP_INSTALL_HEADERS"] = False
+        tc.variables["SKIP_INSTALL_FILES"] = True
+        # Correct for misuse of "${CMAKE_INSTALL_PREFIX}/" in CMakeLists.txt
+        tc.variables["INSTALL_LIB_DIR"] = "lib"
+        tc.variables["INSTALL_INC_DIR"] = "include"
+        tc.generate()
+
+    def _patch_sources(self):
+        apply_conandata_patches(self)
+
+        is_apple_clang12 = self.settings.compiler == "apple-clang" and Version(self.settings.compiler.version) >= "12.0"
+        if not is_apple_clang12:
+            for filename in ['zconf.h', 'zconf.h.cmakein', 'zconf.h.in']:
+                filepath = os.path.join(self.source_folder, filename)
+                replace_in_file(self, filepath,
+                                      '#ifdef HAVE_UNISTD_H    '
+                                      '/* may be set to #if 1 by ./configure */',
+                                      '#if defined(HAVE_UNISTD_H) && (1-HAVE_UNISTD_H-1 != 0)')
+                replace_in_file(self, filepath,
+                                      '#ifdef HAVE_STDARG_H    '
+                                      '/* may be set to #if 1 by ./configure */',
+                                      '#if defined(HAVE_STDARG_H) && (1-HAVE_STDARG_H-1 != 0)')
 
     def build(self):
-        """ Define your project building. You decide the way of building it
-            to reuse it later in any other project.
-        """
+        self._patch_sources()
         cmake = CMake(self)
-        cmake.definitions["BUILD_SHARED_LIBS"] = self.options.shared
         cmake.configure()
         cmake.build()
-        cmake.install()
+
+    def _extract_license(self):
+        tmp = load(self, os.path.join(self.source_folder, "zlib.h"))
+        license_contents = tmp[2:tmp.find("*/", 1)]
+        return license_contents
 
     def package(self):
-        """ Define your conan structure: headers, libs, bins and data. After building your
-            project, this method is called to create a defined structure:
-        """
-        # Copy findZLIB.cmake to package
-        self.copy("FindZLIB.cmake", ".", ".")
-        
+        save(self, os.path.join(self.package_folder, "licenses", "LICENSE"), self._extract_license())
+        cmake = CMake(self)
+        cmake.install()
+
     def package_info(self):
-        if self.settings.os == "Windows":
-            if self.options.shared:
-                if self.settings.build_type == "Debug" and self.settings.compiler == "Visual Studio":
-                    self.cpp_info.libs = ['zlibd']
-                else:
-                    self.cpp_info.libs = ['zlib']
-            else:
-                if self.settings.build_type == "Debug" and  self.settings.compiler == "Visual Studio":
-                    self.cpp_info.libs = ['zlibstaticd']
-                else:
-                    self.cpp_info.libs = ['zlibstatic']
+        self.cpp_info.set_property("cmake_find_mode", "both")
+        self.cpp_info.set_property("cmake_file_name", "ZLIB")
+        self.cpp_info.set_property("cmake_target_name", "ZLIB::ZLIB")
+        self.cpp_info.set_property("pkg_config_name", "zlib")
+        if (self.settings.os == "Windows" or self.settings.os == "WindowsStore") and not self._is_mingw:
+            libname = "zdll" if self.options.shared else "zlib"
         else:
-            self.cpp_info.libs = ['z']
+            libname = "z"
+        self.cpp_info.libs = [libname]
+
+        self.cpp_info.names["cmake_find_package"] = "ZLIB"
+        self.cpp_info.names["cmake_find_package_multi"] = "ZLIB"
